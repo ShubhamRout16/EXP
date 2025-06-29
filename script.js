7a0114233ceac630b918ecff143c04607edde5e4
ShubhamRout16 <routshubham2028@gmail.com>
2025-06-29 16:20:49 +0530

    feat : switching tabs functionality added

---
+++ script.js
@@ -0,0 +1,27 @@
+// feat: tab switching functionality 
+// click on the tab to show its respective page content
+
+
+// all tabs
+const transaction = document.getElementById('transactions')
+const analytics = document.getElementById('analytics')
+const autopay = document.getElementById('autopay')
+const history = document.getElementById('history')
+function showTab(tabName){
+  // removed active class from all the buttons
+  const tabsBtn = document.querySelectorAll('.tab-trigger')
+  tabsBtn.forEach(btn => {
+    return btn.classList.remove('active')
+  });
+  // remove the active class from all the tabcontent
+  transaction.classList.remove('active')
+  analytics.classList.remove('active')
+  autopay.classList.remove('active')
+  history.classList.remove('active')
+  // adding active class to the button name clicked tab btn
+  const chosenElementBtn = document.getElementById(`${tabName}-btn`)
+  const chosenElement = document.getElementById(tabName)
+  chosenElementBtn.classList.add('active')
+  // adding active class to the clicked button tab content
+  chosenElement.classList.add('active')
+}
